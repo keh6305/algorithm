118ea431ce2efee53854726f0e3ea33a8ad1c559
keh6305 <keh6305@gmail.com>
2022-11-23 22:07:33 +0900

    몫구하기 알고리즘

--- algorithm/src/test/java/com/algorithm/programmers/lv0.java
+++ algorithm/src/test/java/com/algorithm/programmers/lv0.java
@@ -1,5 +1,20 @@
 package com.algorithm.programmers;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class lv0
 {
+    // 몫구하기
+    @Test
+    public void findShare()
+    {
+        int num1 = 10;
+        int num2 = 2;
+
+        int result1 = num1 / num2;
+
+        assertThat(result1).isEqualTo(5);
+    }
 }
