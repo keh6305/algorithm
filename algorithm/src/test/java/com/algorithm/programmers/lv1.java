2198f8a915d2302e64d2dbb63c8557ba2cd84812
keh6305 <keh6305@gmail.com>
2022-11-28 02:02:39 +0900

    레벨1 추가

--- algorithm/src/test/java/com/algorithm/programmers/lv1.java
+++ algorithm/src/test/java/com/algorithm/programmers/lv1.java
@@ -79,4 +79,68 @@ public class lv1
             result[i] = Integer.parseInt(String.valueOf(str.charAt(str.length() - (i + 1))));
         }
     }
+
+    // 제곱근 판별
+    @Test
+    public void checkSquare()
+    {
+        long result = 0;
+        int num = 121;
+
+        long sqrt = (long)Math.sqrt(num);
+        long pow = (long)Math.pow(sqrt, 2);
+
+        if(num == pow)
+        {
+            result = (long)Math.pow((sqrt + 1), 2);
+        }
+        else
+        {
+            result = -1;
+        }
+
+        assertThat(result).isEqualTo(144);
+    }
+
+    // 문자열내 문자 구하기
+    @Test
+    public void letterOfString()
+    {
+        String str = "pPoooyY";
+
+        int p = 0;
+        int y = 0;
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(String.valueOf(str.charAt(i)).equals("p") || String.valueOf(str.charAt(i)).equals("P"))
+            {
+                p++;
+            }
+            if(String.valueOf(str.charAt(i)).equals("y") || String.valueOf(str.charAt(i)).equals("Y"))
+            {
+                y++;
+            }
+        }
+
+        assertThat(p).isEqualTo(y);
+    }
+
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i = 0; i < absolutes.length; i++)
+        {
+            if(signs[i] == true)
+            {
+                answer += absolutes[i];
+            }
+            else
+            {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
 }
