c4f9b78e42473f9648f03ff04b464f8648daf89a
keh6305 <keh6305@gmail.com>
2022-11-27 18:43:26 +0900

    알고리즘 추가

--- algorithm/src/test/java/com/algorithm/programmers/lv1.java
+++ algorithm/src/test/java/com/algorithm/programmers/lv1.java
@@ -27,4 +27,56 @@ public class lv1
 
         assertThat(result).isEqualTo(28);
     }
+
+    // 짝수, 홀수
+    @Test
+    public void oddEven()
+    {
+        String result = "";
+        int num = 3;
+
+        if((num % 2) == 0)
+        {
+            result = "Even";
+        }
+        else
+        {
+            result = "Odd";
+        }
+
+        System.out.println("result = " + result);
+    }
+
+    // 자릿수 더하기
+    @Test
+    public void addDigits()
+    {
+        int result = 0;
+        int num = 123;
+
+        while(num > 0)
+        {
+            result += num % 10;
+
+            num = num / 10;
+        }
+
+        assertThat(result).isEqualTo(6);
+    }
+
+    // 자연수 뒤집기
+    @Test
+    public void numberFlip()
+    {
+        long num = 12345;
+        
+        String str = String.valueOf(num);
+
+        int[] result = new int[str.length()];
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            result[i] = Integer.parseInt(String.valueOf(str.charAt(str.length() - (i + 1))));
+        }
+    }
 }
