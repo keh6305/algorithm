3068b02b8454ba2a8b7a6ff4783ec9c168067571
keh6305 <keh6305@gmail.com>
2022-12-15 22:21:11 +0900

    백준 step1 추가

---
+++ algorithm/src/main/java/com/algorithm/baekjoon/step1.java
@@ -0,0 +1,22 @@
+package com.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class step1
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0;
+        int b = 0;
+
+        System.out.print("a = ");
+        a = sc.nextInt();
+
+        System.out.print("b = ");
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
