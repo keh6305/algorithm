19ac12dba295d78fad76de82683a62e6e3ffe3a9
keh6305 <keh6305@gmail.com>
2022-12-15 22:20:51 +0900

    백준 step1 테스트

---
+++ algorithm/src/test/java/com/algorithm/baekjoon/step1.java
@@ -0,0 +1,11 @@
+package com.algorithm.baekjoon;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class step1
+{
+}
