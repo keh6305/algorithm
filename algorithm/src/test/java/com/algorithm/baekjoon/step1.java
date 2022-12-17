0967f9130eb4eba280647120e6b787835d728367
keh6305 <keh6305@gmail.com>
2022-12-17 17:10:33 +0900

    알고리즘 추가

--- algorithm/src/test/java/com/algorithm/baekjoon/step1.java
+++ algorithm/src/test/java/com/algorithm/baekjoon/step1.java
@@ -8,4 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class step1
 {
+    @Test
+    public void addNumber()
+    {
+        int a = 2;
+        int b = 5;
+
+        int result = a + b;
+
+        System.out.println(a + b);
+    }
 }
