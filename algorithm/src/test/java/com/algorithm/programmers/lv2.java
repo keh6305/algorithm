4bc5b13c58dd99cd350df53403cae5e8bcb76566
keh6305 <keh6305@gmail.com>
2023-01-10 18:47:31 +0900

    프로그래머스 lv2 추가

--- algorithm/src/test/java/com/algorithm/programmers/lv2.java
+++ algorithm/src/test/java/com/algorithm/programmers/lv2.java
@@ -321,4 +321,35 @@ public class lv2
 
         System.out.println("result = [" + result[0] + "][" + result[1] +"]");
     }
+
+    // 행렬의 곱셈
+    @Test
+    public void multiplicationArray()
+    {
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+
+        int[][] result = new int[arr1.length][arr2.length];
+        
+        for(int i = 0; i < arr1.length; i++)
+        {
+            for(int j = 0; j < arr2[0].length; j++)
+            {
+                for(int k = 0; k < arr1[0].length; k++)
+                {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        
+        for(int i = 0; i < result.length; i++)
+        {
+            for(int j = 0; j < result[0].length; j++)
+            {
+                System.out.print("[" + result[i][j] + "]");
+            }
+
+            System.out.println();
+        }
+    }
 }
